2003775ea2c0cc54014a50a9ba8a31db5e1a8204
rajsekhar <rajsekhar9655@gmail.com>
2024-09-08 19:18:58 +0530

    new chages

---
+++ 02-BSV2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense-shell"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is....$R FAILED $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is.....$G SUCCESSFULL $N "  | tee -a $LOG_FILE
+    fi
+}
+
+CHECK_ROOT()
+{
+    if [ $USERID -ne 0 ]
+    then 
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+echo "$0 script started executing at $(date)"
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install nodejs"
+
+id expense &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "expense user not exists... $G Creating $N"
+    useradd expense &>>$LOG_FILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "expense user already exists...$Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+
+
